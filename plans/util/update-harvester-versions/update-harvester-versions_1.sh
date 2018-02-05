f6e0caef0c53515d1e7aaafa651f468ccd5572b4
Robin Weiß <row@informatik.uni-kiel.de>
2018-02-05 10:21:36 +0100

    SAI-343 Code Review: Adde shebang and license headers

--- plans/util/update-harvester-versions/update-harvester-versions_1.sh
+++ plans/util/update-harvester-versions/update-harvester-versions_1.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+ 
 # This script is being called by the Bamboo Job https://ci.gerdi-project.de/browse/UTIL-UHV and does the following things:
 #  1. Retrieve the current version of the GeRDI Parent Pom (https://oss.sonatype.org/content/repositories/snapshots/de/gerdi-project/GeRDI-parent/)
 #  2. Update the Harvester Setup Archive, if needed (https://oss.sonatype.org/content/repositories/snapshots/de/gerdi-project/GeRDI-harvester-setup-archive/)
