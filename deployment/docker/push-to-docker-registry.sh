ebe471e643bf762eeeb567b85eae4847142c4887
Robin Weiß <row@informatik.uni-kiel.de>
2018-07-30 16:02:13 +0200

    SAI-1114 Added a script for deploying docker files

---
+++ deployment/docker/push-to-docker-registry.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Copyright © 2018 Robin Weiss (http://www.gerdi-project.de/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Description:
+# This script builds and pushes a Docker image. After the image is built, 
+# it is pushed to a Docker registry and subsequently removed from the local image list.
+#
+# Arguments:
+# 1 - The URL of the Docker registry to which the image is pushed.
+# 2 - The name of the Docker image that is to be created.
+# 3 - The tag of the built Docker image.
+#
+
+
+# treat unset variables as an error when substituting, because the argument is optional
+set -u
+
+
+#########################
+#  FUNCTION DEFINITIONS #
+#########################
+
+# Pushes a specified docker image to the registry and tags it with latest and a specified tag.
+# Subsequently, the image is removed from the local image list to free up space.
+#
+# Arguments:
+#  1 - the Docker Registry URL
+#  2 - the name of the Docker image to be built, excluding any tags
+#  3 - the tag of the Docker image to be built
+#
+DockerPush() {
+  local registryUrl="$1"
+  local imageName="$2"
+  local imageTag="$3"
+  
+  local image
+  image="$registryUrl/$imageName:$imageTag"
+  
+  echo "Pushing docker image: $image" >&2
+  docker push "$image"
+  
+  # push 'latest' tag
+  
+  if [ "$imageTag" != "latest" ]; then
+    local latestImage
+	latestImage="$registryUrl/$imageName:latest"
+	
+    docker tag  "$image" "$latestImage"
+    docker push "$latestImage"
+  fi
+
+  # remove image from local image list
+  echo "Removing docker image from local docker image list." >&2
+  docker rmi "$image"
+}
+
+
+# Builds a Docker image.
+#
+# Arguments:
+#  1 - the Docker Registry URL
+#  2 - the name of the Docker image to be built, excluding any tags
+#  3 - the tag of the Docker image to be built
+#
+DockerBuild() {
+  local registryUrl="$1"
+  local imageName="$2"
+  local imageTag="$3"
+  
+  local image
+  image="$registryUrl/$imageName:$imageTag"
+
+  # build image
+  echo "Building docker image $image." >&2
+  docker build -t "$image" .
+}
+
+
+# If there are warfiles to be deployed, change their access rights 
+# in order to allow jetty to use them.
+#
+# Arguments: -
+#
+AllowWarFileAccess() {
+  local warFile
+  warFile=$(ls -cB "target/"*.war | head -1)
+  
+  if [ -n "$warFile" ]; then
+    stat -c "%a" $warFile
+    chmod o+rw $warFile
+  fi
+}
+
+
+# The main function that is called on script execution.
+#
+Main() {
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "You need to specify three arguments: dockerRegistryURL, dockerImageName, dockerImageTag " >&2
+    exit 1
+  fi
+  
+  local dockerRegistryUrl="$1"
+  local dockerImageName="$2"
+  local dockerImageTag="$3"
+  
+  AllowWarFileAccess
+  DockerBuild "$dockerRegistryUrl" "$dockerImageName" "$dockerImageTag"
+  DockerPush "$dockerRegistryUrl" "$dockerImageName" "$dockerImageTag"
+}
+
+
+###########################
+#  BEGINNING OF EXECUTION #
+###########################
+
+Main "$@"
